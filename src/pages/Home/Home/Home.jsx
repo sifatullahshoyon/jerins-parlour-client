7f44fe6bcf46ab6307d27f3e7ec3373c544e1e57
freelancersifat627e <freelancersifat627@gmail.comm>
2024-06-04 17:09:55 +0600

    complete services section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from '../../../components/Title';
 import Header from '../Header';
+import Services from '../Services';
 
 const Home = () => {
     return (
         <div>
             <Title title="Home" />   
             <Header />
+            <Services />
         </div>
     );
 };
