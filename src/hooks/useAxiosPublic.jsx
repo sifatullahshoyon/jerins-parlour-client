b75154e34cf14e2a4bec6294b8840bedbf618792
freelancersifat627e <freelancersifat627@gmail.comm>
2024-07-30 19:30:38 +0600

    deployment successfully

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://jerins-parlour-server-sooty.vercel.app'
 });
 
 const useAxiosPublic = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProviders";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://jerins-parlour-server-sooty.vercel.app",
 });
 
 const useAxiosSecure = () => {
